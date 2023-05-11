35f9a04613cb5a80e445abea461e6fcedd9e4343
Ewerton Cordeiro <ewertonecordeiro@gmail.com>
2023-05-11 00:04:12 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_game")
 
-
 public class Game {
 	
 	@Id
@@ -23,23 +22,29 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
-	private String imgUr;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	private Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUr,
-			String shortDescription, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, 
+			String imgUrl, String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
-		this.imgUr = imgUr;
+		this.platforms = platforms;
+		this.score = score;
+		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
@@ -77,19 +82,27 @@ public class Game {
 	}
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
+	}
+	
+	public void setPlatform(String platform) {
+		this.platforms = platform;
+	}
+	
+	public Double getScore() {
+		return score;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUr() {
-		return imgUr;
+		return imgUrl;
 	}
 
 	public void setImgUr(String imgUr) {
-		this.imgUr = imgUr;
+		this.imgUrl = imgUr;
 	}
 
 	public String getShortDescription() {
