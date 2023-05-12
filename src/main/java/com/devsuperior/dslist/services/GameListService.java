cfc17195a942094f108848b0206b5e058df4ce31
Ewerton Cordeiro <ewertonecordeiro@gmail.com>
2023-05-11 21:50:49 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class GameListService {
+	
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
+	public List<GameListDTO> findAll(){
+		List<GameList> result = gameListRepository.findAll();
+		return result.stream().map(x -> new GameListDTO(x)).toList();
+		
+	}
+	
+	
+
+}
